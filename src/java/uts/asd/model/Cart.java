995468e34c01d2192eb59460ce2d7774001eb45d
hannokohnk <89053042+hannokohnk@users.noreply.github.com>
2021-10-05 03:17:47 +1100

    Add files via upload

---
+++ src/java/uts/asd/model/Cart.java
@@ -0,0 +1,66 @@
+package uts.asd.model;
+
+import cart.util.BookException;
+import cart.util.IdVerifier;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+
+@Stateful
+public class CartBean implements Cart {
+    String customerName;
+    String customerId;
+    List<String> contents;
+
+    public void initialize(String person) throws BookException {
+        if (person == null) {
+            throw new BookException("Null person not allowed.");
+        } else {
+            customerName = person;
+        }
+
+        customerId = "0";
+        contents = new ArrayList<String>();
+    }
+
+    public void initialize(String person, String id)
+                 throws BookException {
+        if (person == null) {
+            throw new BookException("Null person not allowed.");
+        } else {
+
+            customerName = person;
+        }
+
+        IdVerifier idChecker = new IdVerifier();
+
+        if (idChecker.validate(id)) {
+            customerId = id;
+        } else {
+            throw new BookException("Invalid id: " + id);
+        }
+
+        contents = new ArrayList<String>();
+    }
+
+    public void addBook(String title) {
+        contents.add(title);
+    }
+
+    public void removeBook(String title) throws BookException {
+        boolean result = contents.remove(title);
+        if (result == false) {
+            throw new BookException(title + " not in cart.");
+        }
+    }
+
+    public List<String> getContents() {
+        return contents;
+    }
+
+    @Remove
+    public void remove() {
+        contents = null;
+    }
+}
