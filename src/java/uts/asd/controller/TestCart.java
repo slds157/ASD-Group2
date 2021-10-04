177db7461b16b2f5d24c60760327251d60e0f72e
hannokohnk <89053042+hannokohnk@users.noreply.github.com>
2021-10-05 03:17:13 +1100

    Add files via upload

---
+++ src/java/uts/asd/controller/TestCart.java
@@ -0,0 +1,16 @@
+package TestCart.java;
+
+import cart.util.BookException;
+import java.util.List;
+import javax.ejb.Remote;
+
+@Remote
+public interface Cart {
+    public void initialize(String person) throws BookException;
+    public void initialize(String person, String id)
+         throws BookException;
+    public void addBook(String title);
+    public void removeBook(String title) throws BookException;
+    public List<String> getContents();
+    public void remove();
+}
